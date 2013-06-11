10dcb6818cadafd069db2b67b6838796fea2b3da
Pat Allan <pat@freelancing-gods.com>
2013-06-11 21:52:34 +1000

    A starting point.
    Nothing beyond Rails templates thus far.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Silvergum::Application.config.secret_key_base = '7dfa3ee3a84f10a028765f111a0d92d7d79bccac63dc641a223b94773af40aae0a74487f114ce6ce0a3b27d70a369118c729047e2346f85e253cdc072a949e3b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Silvergum::Application.config.session_store :cookie_store, key: '_silvergum_session'
